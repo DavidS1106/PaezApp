cad407f05731329d41d3b5bebce672b030303cf5
davidS <david.sabo@hotmail.be>
2021-05-10 02:35:13 +0200

    gg

--- src/components/forms/CategoriesFormContainer.jsx
+++ src/components/forms/CategoriesFormContainer.jsx
@@ -157,8 +157,8 @@ class CategoriesFormContainer extends React.Component {
       
     async handleFormAdd(e){
         let form=e.target
-        e.preventDefault();
-        
+        //e.preventDefault();
+        e.stopPropagation();
         //transform to base64
         const toBase64 = file => new Promise((resolve, reject) => {
           const reader = new FileReader();
@@ -168,8 +168,9 @@ class CategoriesFormContainer extends React.Component {
           reader.onerror = error => reject(error);
         });
         let base64=await toBase64(e.target.img.files[0]);
+        let toPost={author:{id:this.state.id_artist},cat:form.categorie.value,support:form.support.value,imgUri:base64,name:form.titre.value,year:form.annee.value,price:0}
         axios.post(
-        'https://paezappsabo.herokuapp.com/tableaux/insert',{author:{id:this.state.id_artist},cat:form.categorie.value,support:form.support.value,imgUri:base64,name:form.titre.value,year:form.annee.value,price:0},AuthService.getHeader())
+        'https://paezappsabo.herokuapp.com/tableaux/insert',toPost,AuthService.getHeader())
         .then(result =>{
           console.log(result);
           this.fetchingImages();
